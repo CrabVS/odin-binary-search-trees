33487bc43838e83ddf923731aa5d54a34a8ce384
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-08-11 19:37:39 +0200

    Add node and tree classes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('works');
+class Node {
+    constructor(value) {
+        this.value = value || null;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    buildTree(array) {
+        console.log(array);
+    }
+
+    constructor(array) {
+        this.root = this.buildTree(array);
+    }
+
+}
+
+const bigTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
